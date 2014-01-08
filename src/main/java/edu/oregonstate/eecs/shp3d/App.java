09e3dfa674b8d845c1e709371a24a6717b00b06a
ratanasv <ratanasv@eecs.oregonstate.edu>
2014-01-07 18:56:31 -0800

    refactored, and added a write method.

--- src/main/java/edu/oregonstate/eecs/shp3d/App.java
+++ src/main/java/edu/oregonstate/eecs/shp3d/App.java
@@ -2,21 +2,31 @@ package edu.oregonstate.eecs.shp3d;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.Serializable;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 import org.apache.commons.cli.ParseException;
 import org.apache.commons.io.FilenameUtils;
+import org.geotools.data.DefaultTransaction;
 import org.geotools.data.FeatureReader;
 import org.geotools.data.FeatureSource;
+import org.geotools.data.Transaction;
 import org.geotools.data.shapefile.ShapefileDataStore;
+import org.geotools.data.shapefile.ShapefileDataStoreFactory;
 import org.geotools.data.simple.SimpleFeatureIterator;
+import org.geotools.data.simple.SimpleFeatureSource;
+import org.geotools.data.simple.SimpleFeatureStore;
 import org.geotools.feature.FeatureCollection;
 import org.geotools.feature.FeatureIterator;
+import org.geotools.swing.data.JFileDataStoreChooser;
 import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
 import org.opengis.feature.type.FeatureType;
 import org.opengis.geometry.Geometry;
 
@@ -30,40 +40,45 @@ public class App  {
 		return FilenameUtils.removeExtension(shpFile).concat(extension);
 	}
 	
+	private static File getNewShapeFile(File twoDSHPFile) {
+        String path = twoDSHPFile.getAbsolutePath();
+        String newPath = path.substring(0, path.length() - 4) + "3D.shp";
 
-	public static void main( String[] args ) throws IOException {
-		System.out.println( "shp3d..." );
-		try {
-			ArgumentManager.Init(args);
-		} catch (ParseException e) {
-			e.printStackTrace();
-			System.exit(1);
-		}
-		
-		URL shapeURL = null;
+        JFileDataStoreChooser chooser = new JFileDataStoreChooser("shp");
+        chooser.setDialogTitle("Save shapefile");
+        chooser.setSelectedFile(new File(newPath));
+
+        int returnVal = chooser.showSaveDialog(null);
+
+        if (returnVal != JFileDataStoreChooser.APPROVE_OPTION) {
+            throw new RuntimeException("output file is not valid");
+        }
+
+        File threeDSHPFile = chooser.getSelectedFile();
+        if (threeDSHPFile.equals(twoDSHPFile)) {
+            throw new RuntimeException("Error: cannot replace " + twoDSHPFile);
+        }
+
+        return threeDSHPFile;
+    }
+	
+	private static File getExistingShapeFile() {
 		JFileChooser fileChooser = new JFileChooser();
 		fileChooser.setFileFilter(new FileNameExtensionFilter("Shapefile", "shp"));
 		int result = fileChooser.showOpenDialog(null);
 		 
 		if (result == JFileChooser.APPROVE_OPTION) {
 		    File f = fileChooser.getSelectedFile();
-		    shapeURL = f.toURL();
+		    return f;
 		} else {
 		    throw new RuntimeException("no valid input 2D shapefile");
 		}
-		
-		ShapefileDataStore store = new ShapefileDataStore(shapeURL);
-		String name = store.getTypeNames()[0];
-		FeatureSource source = store.getFeatureSource(name);
-		FeatureCollection fsShape = source.getFeatures();
-
-		// print out a feature type header and wait for user input
-		FeatureType ft = source.getSchema();
-		System.out.println("FID\t");
-		System.out.println(ft.toString());
-		System.out.println();
-
-		FeatureIterator<SimpleFeature> iterator = (SimpleFeatureIterator) fsShape.features();
+	}
+	
+	private static void printFeaturesAttributes(
+		FeatureCollection<SimpleFeatureType, SimpleFeature> fsShape) 
+	{
+		FeatureIterator<SimpleFeature> iterator = fsShape.features();
 		try {
 			while( iterator.hasNext() ){
 				SimpleFeature feature = iterator.next();
@@ -79,8 +94,83 @@ public class App  {
 		finally {
 			iterator.close();
 		}
+	}
+	
 
-		iterator = (SimpleFeatureIterator) fsShape.features();
+	public static void main( String[] args ) throws IOException {
+		System.out.println( "shp3d..." );
+		try {
+			ArgumentManager.Init(args);
+		} catch (ParseException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		
+		
+		File existingSHPFile = getExistingShapeFile();
+		ShapefileDataStore store = new ShapefileDataStore(existingSHPFile.toURI().toURL());
+		String name = store.getTypeNames()[0];
+		FeatureSource<SimpleFeatureType, SimpleFeature> source = store.getFeatureSource(name);
+		FeatureCollection<SimpleFeatureType, SimpleFeature> fsShape = source.getFeatures();
+
+		// print out a feature type header and wait for user input
+		SimpleFeatureType featureType = (SimpleFeatureType) source.getSchema();
+		System.out.println("FID\t");
+		System.out.println(featureType.toString());
+		System.out.println();
+
+		printFeaturesAttributes(fsShape);
+
+		printFeaturesGeometry(fsShape);
+		
+		File newFile = getNewShapeFile(existingSHPFile);
+
+        ShapefileDataStoreFactory dataStoreFactory = new ShapefileDataStoreFactory();
+
+        Map<String, Serializable> params = new HashMap<String, Serializable>();
+        params.put("url", newFile.toURI().toURL());
+        params.put("create spatial index", Boolean.TRUE);
+
+        ShapefileDataStore newDataStore = (ShapefileDataStore) dataStoreFactory.createNewDataStore(params);
+        newDataStore.createSchema(featureType);
+
+        /*
+         * You can comment out this line if you are using the createFeatureType method (at end of
+         * class file) rather than DataUtilities.createType
+         */
+        //newDataStore.forceSchemaCRS(DefaultGeographicCRS.WGS84);
+		
+		Transaction transaction = new DefaultTransaction("create");
+
+        String typeName = store.getTypeNames()[0];
+        SimpleFeatureSource featureSource = store.getFeatureSource(typeName);
+
+        if (featureSource instanceof SimpleFeatureStore) {
+            SimpleFeatureStore featureStore = (SimpleFeatureStore) featureSource;
+
+            featureStore.setTransaction(transaction);
+            try {
+                featureStore.addFeatures(fsShape);
+                transaction.commit();
+
+            } catch (Exception problem) {
+                problem.printStackTrace();
+                transaction.rollback();
+
+            } finally {
+                transaction.close();
+            }
+        } else {
+            System.out.println(typeName + " does not support read/write access");
+            System.exit(1);
+        }
+
+	}
+
+	private static void printFeaturesGeometry(
+		FeatureCollection<SimpleFeatureType, SimpleFeature> fsShape) 
+	{
+		FeatureIterator<SimpleFeature> iterator = fsShape.features();
 		try {
 			while( iterator.hasNext() ){
 				SimpleFeature feature = iterator.next();
@@ -93,6 +183,6 @@ public class App  {
 		finally {
 			iterator.close();
 		}
-
 	}
+
 }
