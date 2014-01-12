2bdd03bc1b1849ada9f05d3c9dd05aa0e096a03f
ratanasv <ratanasv@eecs.oregonstate.edu>
2014-01-12 14:45:01 -0800

    make the test more rigorous.

--- src/test/java/edu/oregonstate/eecs/shp3d/DEMConnectionTest.java
+++ src/test/java/edu/oregonstate/eecs/shp3d/DEMConnectionTest.java
@@ -12,12 +12,18 @@ public class DEMConnectionTest {
 	public void testConnection() throws IOException {
 		final int numLats = 10;
 		final int numLngs = 10;
+		final float minLat = 40.95f;
+		final float maxLat = 41.0f;
+		final float minLng = -123.0f;
+		final float maxLng = -122.95f;
+		final float epsilon = 1e-8f;
+		
 
 		String urlString = DEMQueryBuilder.startBuilding(MIKE_DEM)
-				.withLat1(40.95f)
-				.withLat2(41.0f)
-				.withLng1(-122.95f)
-				.withLng2(-123.0f)
+				.withLat1(minLat)
+				.withLat2(maxLat)
+				.withLng1(minLng)
+				.withLng2(maxLng)
 				.withNumLats(numLats)
 				.withNumLngs(numLngs)
 			.build();
@@ -26,6 +32,10 @@ public class DEMConnectionTest {
 		DEMConnection.XTRHeader header = connection.getXTRHeader();
 		Assert.assertEquals(numLats, header.numLats);
 		Assert.assertEquals(numLngs, header.numLngs);
+		Assert.assertEquals(minLat, header.minLat, epsilon);
+		Assert.assertEquals(maxLat, header.maxLat, epsilon);
+		Assert.assertEquals(minLng, header.minLng, epsilon);
+		Assert.assertEquals(maxLng, header.maxLng, epsilon);
 
 		for (int i=0; i<header.numLats; i++) {
 			for (int j=0; j<header.numLngs; j++) {
